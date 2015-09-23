9bb8a29e36dedb52cc135092f1cb6a14e4342216
Egor Gorbunov <egor-mailbox@ya.ru>
2015-09-23 17:35:20 +0300

    Add 4 linux regex solution

--- linux/hwlec1/4.sh
+++ linux/hwlec1/4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -r -e \
-'s/^[ \t]*#include[ ]?<([^>]*)>[ \t]*/\1/;'\
-'s/^[ \t]*#include[ ]?"([^"]*)"[ \t]*/\1/' test4.cpp
+sed -r -n -e \
+'s/^\s*#include[ ]?<([^>]*)>\s*/\1/p;'\
+'s/^\s*#include[ ]?"([^"]*)"\s*/\1/p' test4.cpp
