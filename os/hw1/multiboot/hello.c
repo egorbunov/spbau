57bf4d9d0fdd4621575f85bc930cd3bb35cf7b2c
Egor Gorbunov <egor-mailbox@ya.ru>
2015-09-27 14:28:21 +0300

    Add working Multiboot bootloader

--- os/hw1/multiboot/hello.c
+++ os/hw1/multiboot/hello.c
@@ -1,9 +1,23 @@
-#include "printf.h"
-#include "screen.h"
-#include "types.h"
 
-void main(void)
-{   
-    clear_screen();
-    printf("\n>>> Hello World!\n");
+int next_color() {
+    static int color = 0x06;
+    color = (color + 0x01) % 16;
+    color = (color == 0) ? 0x01 : color;
+    return color;
+}
+
+
+void cmain(void) {
+    const int COLUMN_NUM = 0xB80A0 - 0xB8000;
+    const int LINE_INDEX = 11;
+    const int SHIFT = 31;
+
+    char* video = (char*) 0xB8000 + COLUMN_NUM * LINE_INDEX + SHIFT * 2;
+
+    const char hw[] = "Hello World!";
+    int i;
+    for (i = 0; hw[i] != '\0'; i++) {
+        video[i * 2] = hw[i];
+        video[i * 2 + 1] = next_color();
+    }
 }
