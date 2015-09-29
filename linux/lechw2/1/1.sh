78bea9d17aea84fe0bfecb8fc0262f874aaf5a07
Egor Gorbunov <egor-mailbox@ya.ru>
2015-09-29 20:19:08 +0000

    add 1 linux task

--- linux/lechw2/1/1.sh
+++ linux/lechw2/1/1.sh
@@ -4,13 +4,19 @@ echo "Need to get sections table from MBR (/dev/sda). It's root protected, so pl
 sudo dd if=/dev/sda ibs=1 skip=446 count=64 of=tmp_sectors status=none
 
 let "CNT=0"
+let "ind=0"
 for i in $(seq 0 16 63);
 do
 	res=$(dd if=tmp_sectors ibs=1 skip=$i count=16 status=none)
 	if [ -n "$res" ]; then
 		let "CNT+=1"
+		let "type_shift=i+4"
+		echo "------------------ SECTION with index $ind ----------------- "
+	    part_type=$(dd if=tmp_sectors ibs=1 skip=$type_shift count=1 status=none | hexdump -v -e '"0x" 1/1 "%02X" "\n"')
+	    cat "part_types.txt" | grep "$part_type"
 	fi
+	let "ind+=1"
 done
-
+echo "----------------------------------------------- "
 echo "Number of sections = $CNT"
 sudo rm tmp_sectors
