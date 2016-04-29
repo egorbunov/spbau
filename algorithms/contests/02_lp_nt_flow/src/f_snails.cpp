7926f6fabd4d5b2c8cfd31086fa0d019e3c3b934
Egor Gorbunov <egor-mailbox@ya.ru>
2016-04-29 14:03:30 +0300

    Add snails algo task solution

--- algorithms/contests/02_lp_nt_flow/src/f_snails.cpp
+++ algorithms/contests/02_lp_nt_flow/src/f_snails.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <climits>
 #include <limits>
+#include <iomanip>
+#include <cassert>
+#include <random>
 
 using namespace std;
 
@@ -15,8 +17,10 @@ struct network_t {
         int to;
         int flow;    // flow
         int c;    // capacity
+        int id;
+        bool used; // for k paths searching
         edge_t(int next, int from, int to, int f, int c):
-                next(next), from(from), to(to), flow(f), c(c)
+                next(next), from(from), to(to), flow(f), c(c), used(false)
         {}
     };
 
@@ -28,16 +32,15 @@ struct network_t {
     int s; // sink
     int t; // target
 
-    network_t(int n, int m): head(n, -1), is_used(n, 0), used_flag(1), s(0), t(n - 1) {
+    vector<vector<int>> paths;
+
+    network_t(int n, int m, int s, int t): head(n, -1), is_used(n, 0), used_flag(1), s(s), t(t) {
     }
 
-    /**
-     * Add undirected edge
-     */
-    void add_edge(int a, int b, int capacity) {
+    void add_d_edge(int a, int b, int capacity) {
         edges.push_back(edge_t(head[a], a, b, 0, capacity));
         head[a] = (int) edges.size() - 1;
-        edges.push_back(edge_t(head[b], b, a, 0, capacity));
+        edges.push_back(edge_t(head[b], b, a, 0, 0));
         head[b] = (int) edges.size() - 1;
     }
 
@@ -50,7 +53,47 @@ struct network_t {
         return res;
     }
 
+    int get_k_paths(int k) {
+        int res = 0;
+        int f;
+        while ((f = inc_flow(s, std::numeric_limits<int>::max())) > 0) {
+            res += f;
+            used_flag += 1;
+            if (res >= k) {
+                break;
+            }
+        }
+
+        if (res < k) {
+            return 0;
+        }
+
+        paths = vector<vector<int>>(k, vector<int>(1, s));
+        for (int i = 0; i < k; ++i) {
+            used_flag += 1;
+            get_path(s, paths[i]);
+            assert(paths[i][paths[i].size() - 1] == t);
+        }
+
+        return 1;
+    }
+
 private:
+    void get_path(int v, vector<int>& path) {
+        if (v == t) return;
+
+        for (int i = head[v]; i != -1; i = edges[i].next) {
+            if (edges[i].flow > 0 && edges[i].c > 0 && !edges[i].used) {
+
+                edges[i].used = true;
+                path.push_back(edges[i].to);
+
+                get_path(edges[i].to, path);
+                return;
+            }
+        }
+    }
+
     /**
      * Finds augmented path recursively and tries to increases flow by delta
      */
@@ -71,24 +114,35 @@ private:
     }
 };
 
+
 int main() {
-    int n, m;
+    int n, m, s, t;
 
-    cin >> n >> m;
+    cin >> n >> m >> s >> t;
 
-    network_t net(n, m);
+    network_t net(n, m, s - 1, t - 1);
 
     for (int i = 0; i < m; ++i) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        net.add_edge(a - 1, b - 1, c);
+        int a, b;
+        cin >> a >> b;
+        if (a == b) {
+            continue; // loops doesn't matter
+        }
+        net.add_d_edge(a - 1, b - 1, 1);
     }
 
-    int flow = net.calc_max_flow();
-
-    cout << flow << endl;
-    for (int i = 0; i < m; ++i) {
-        cout << net.edges[2 * i].flow << endl;
+    int k = 2;
+    int ans = net.get_k_paths(k);
+    if (ans) {
+        cout << "YES" << endl;
+        for (int i = 0; i < k; ++i) {
+            for (int v : net.paths[i]) {
+                cout << v + 1 << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "NO" << endl;
     }
 
     return 0;
