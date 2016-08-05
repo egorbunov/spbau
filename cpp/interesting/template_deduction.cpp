367f9b87484906a335177848c59c7718bad398c5
Egor Gorbunov <egor-mailbox@ya.ru>
2016-08-05 19:34:12 +0300

    Add changes

--- cpp/interesting/template_deduction.cpp
+++ cpp/interesting/template_deduction.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <typeinfo>
+
+int fun()
+{
+	return 0;
+}
+
+typedef int (*pfun_t)();
+
+template<class T>
+void foo(T* p)
+{
+	std::cout << "T : " << typeid(T).name() << std::endl;
+	std::cout << "T*: " << typeid(p).name() << std::endl;
+}
 
 int main() {
+	pfun_t pf = &fun;
+	foo(pf);
+	int **a = new int*[10];
+	foo(&a);
 	return 0;
 }
